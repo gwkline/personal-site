7c55ef0a1938024155537cf1ea38f6666a9817fd
gwkline <77706736+gwkline@users.noreply.github.com>
2023-06-07 14:25:56 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Gavin Kline</title>
+	<meta name="description" content="Personal Website and Showcase" />
+</svelte:head>
+
+<div class="container py-7 flex flex-col outline-1 px-5 mx-auto">
+	<span>Hi there,</span>
+	<span
+		>My name is Gavin Kline, I'm a software engineer currently based out of:
+	</span>
+	<span>New York, NY 🇺🇸 🗽</span>
+	<br />
+	<span
+		>I haven't finished this site yet... but you can check out my work on my <a
+			href="https://github.com/gwkline">GitHub</a
+		>
+		or my
+		<a href="https://www.linkedin.com/in/gavinkline/">LinkedIn</a></span
+	>
+</div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,3 @@
+<script lang="ts"></script>
+
+<div>Under construction...</div>
